23f07b9b45179959d3f44e42936958d96dfe68a7
CassianoCardoso <dj_kassiano@hotmail.com>
2022-06-26 22:23:06 -0300

    Herança e Plimorfismo

---
+++ Banco/Entities/BusinessAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Entities
+{
+    internal class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+                                                                                            // Usando o construtor da classe Account
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount) //Empréstimo
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ Banco/Entities/SavingsAcconunt.cs
@@ -0,0 +1,30 @@
+﻿using Banco.Entities;
+
+namespace Banco.Entities
+{
+    internal class SavingsAcconunt : Account
+    {
+        public double InterestRate { get; set; }// taxa juros
+
+        public SavingsAcconunt()
+        {
+        }
+
+        public SavingsAcconunt(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()// Atualizar saldo
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+           base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+
+    }   
+}
